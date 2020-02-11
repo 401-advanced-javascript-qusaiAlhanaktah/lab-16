6708115a74c02ecacc56b166e55acd413077a56e
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2020-02-12 00:27:08 +0200

    fix the requirement

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,11 +1,23 @@
+/* eslint-disable strict */
+
+'use strict';
+
 
 module.exports = exports = {};
 
+
+
 exports.readFile = (file, cb) => {
-  if( file.match(/bad/i) ) {
+  if (file.match(/bad/i)) {
     cb('Invalid File');
   }
-  else {
-    cb(undefined, new Buffer('File Contents'));
+};
+
+
+
+exports.writeFile = (file, buffer, cb) => {
+  if (file.match(/bad/i)) {
+    cb('Invalid file');
+    return Buffer.from('File Contents');
   }
 };
--- index.js
+++ index.js
@@ -1,17 +1,49 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable strict */
 'use strict';
 
 const fs = require('fs');
+const event = require('./src/events.js');
+
+require('./src/logger.js');
+require('./src/error.js');
 
 const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
-    let text = data.toString().toUpperCase();
-    fs.writeFile( file, Buffer.from(text), (err, data) => {
-      if(err) { throw err; }
-      console.log(`${file} saved`);
+  readFile(file)
+    .then(data => {
+      writeFile(file, changeCase(data));
+    });
+};
+// convert to Upper Case
+function changeCase(data) {
+  return data.toUpperCase();
+}
+// reading the file
+function readFile(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) {
+        event.emit('error', 'readFile error', `${err}`);
+      }
+      event.emit('log', 'readFile', `${file} saved`);
+      resolve(data.toString());
+    });
+
+  });
+
+}
+// edit on file
+function writeFile(file, text) {
+  return new Promise((resolve, reject) => {
+
+    fs.writeFile(file, Buffer.from(text), (err, data) => {
+      if (err) {
+        event.emit('error', 'writeFile error', `${err}`);
+      }
+      resolve(event.emit('log', 'writeFile', `${file} saved`));
     });
   });
-};
+}
 
 let file = process.argv.slice(2).shift();
 alterFile(file);
